0651b3a71d28de20837933ab940005b209f3b211
Steven McGinnis <81644391+Steven-McGinnis@users.noreply.github.com>
2023-07-25 20:19:01 -0400

    Additions

--- behavior_delete_on_click.py
+++ behavior_delete_on_click.py
@@ -8,10 +8,11 @@ class DeleteOnClick(Behavior):
 
     def clicked(self):
         if self.game_object.id in GameLogic.game_objects:  # type: ignore
-            identifier = self.game_object.identifier
+            identifier = self.game_object.identifier # type: ignore
             if identifier == "power_up":
+                print("POWER UP")
                 pub.sendMessage("power_up")
 
             # This sends a message to delete from the view 
-            pub.sendMessage("delete", id=self.game_object.id)
+            pub.sendMessage("delete", id=self.game_object.id) # type: ignore
 
---
+++ behavior_gain_speed.py
@@ -0,0 +1,27 @@
+from behavior import Behavior
+from game_logic import GameLogic
+from pubsub import pub
+from behavior_key_move import KeyMove
+
+class GainSpeed(Behavior):
+    def __init__(self):
+        super(GainSpeed, self).__init__()
+        pub.subscribe(self.gain_speed, "power_up")
+        self.count = 0
+        self.speedup = False
+
+    def gain_speed(self):
+        for behavior in self.game_object.behaviors:
+            if isinstance(behavior, KeyMove):
+                behavior.speed += .5
+                self.speedup = True
+
+    def tick(self):
+        if self.speedup:
+            self.count += 1
+            if self.count == 100:
+                for behavior in self.game_object.behaviors:
+                    if isinstance(behavior, KeyMove):
+                        behavior.speed -= .5
+                self.count = 0
+                self.speedup = False
