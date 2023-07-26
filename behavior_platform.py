0715af4b85bac537a1e87dd500000dec444b289d
Steven McGinnis <81644391+Steven-McGinnis@users.noreply.github.com>
2023-07-26 16:50:07 -0400

    Platform Update

---
+++ behavior_platform.py
@@ -0,0 +1,74 @@
+from behavior import Behavior
+from game_logic import GameLogic
+import numpy as np
+
+class Platform(Behavior):
+    def __init__(self, finish, speed, transport):
+        super(Platform, self).__init__()
+
+        self.start = None
+        self.finish = np.array(finish)
+        self.speed = speed
+        self.transport = transport
+
+    def connect(self, game_object):
+        super(Platform, self).connect(game_object)
+        self.start = np.array(self.game_object.position)
+        self.distance = np.linalg.norm(self.finish - self.start)
+        self.direction_vector = (self.finish - self.start) / self.distance
+
+    def tick(self):
+            current = np.array(self.game_object.position)
+            distance = np.linalg.norm(current - self.start)
+
+            riders = []
+            if self.transport:
+                self.game_object.position[1] += 0.1
+
+                for other in GameLogic.game_objects:
+                    if not GameLogic.collide(self.game_object, GameLogic.game_objects[other]):
+                        continue
+
+                    if GameLogic.game_objects[other] in self.game_object.collisions:
+                        continue
+
+                    riders.append(GameLogic.game_objects[other])
+                    
+                self.game_object.position[1] -= 0.1
+
+            already_moved = set()
+            if distance >= self.distance:
+                self.direction_vector = - (1/np.linalg.norm(self.direction_vector))*self.direction_vector
+                self.game_object.position = self.finish
+                temp = self.start
+                self.start = self.finish
+                self.finish = temp
+            else:
+                self.game_object.position = self.start+(distance+self.speed)*self.direction_vector
+                self.move_riders(riders, self.direction_vector, self.speed, already_moved)
+
+            self.game_object._moved = True
+
+    def move_riders(self, riders, direction_vector, speed, already_moved):
+        for rider in riders:
+            if rider in already_moved:
+                continue
+            rider.position = rider.position + speed*direction_vector
+            rider._moved = True
+            already_moved.add(rider)
+
+            other_riders = []
+            for other in GameLogic.game_objects:
+                if other == rider or other == self.game_object:
+                    continue
+
+                if not GameLogic.collide(rider, GameLogic.game_objects[other]):
+                    continue
+
+                if GameLogic.game_objects[other] in rider.collisions:
+                    continue
+
+                other_riders.append(GameLogic.game_objects[other])
+
+            if other_riders:
+                self.move_riders(other_riders, direction_vector, speed, already_moved)
